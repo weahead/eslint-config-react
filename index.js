9cb4255655f753191f2758555ebc442517efa5ef
Michael Zetterberg fd. Lopez <michael@weahead.se>
2019-12-25 10:46:48 +0100

    feat: add support for jest

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 module.exports = {
+  parser: 'babel-eslint',
   env: {
     browser: true,
-    node: true,
     es6: true,
+    node: true,
   },
-  parser: 'babel-eslint',
   plugins: [
     'babel',
     'jsx-a11y',
     'react',
     'react-hooks',
-    'eslint-plugin-import',
-    'eslint-plugin-simple-import-sort',
+    'import',
+    'simple-import-sort',
   ],
   extends: [
     'eslint:recommended',
@@ -63,9 +63,13 @@ module.exports = {
     ],
     'simple-import-sort/sort': 'error',
     'sort-imports': 'off',
+    'import/order': 'off',
     'import/first': 'error',
     'import/newline-after-import': 'error',
     'import/no-duplicates': 'error',
+    'import/no-useless-path-segments': ['error', { commonjs: true }],
+    'import/export': 'error',
+    'import/no-mutable-exports': 'error',
   },
   settings: {
     react: {
@@ -80,6 +84,22 @@ module.exports = {
       rules: {
         'simple-import-sort/sort': 'off',
         'import/order': ['error', { 'newlines-between': 'always' }],
+      },
+    },
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true, // now **/*.test.js files' env has both es6 *and* jest
+      },
+      // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
+      // "extends": ["plugin:jest/recommended"]
+      plugins: ['jest'],
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
       },
     },
   ],
